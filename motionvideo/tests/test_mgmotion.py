7d35195ea44016847e3c76f6d20487f71f12ff6a
Marcus Widmer <marcus.widmer@fys.uio.no>
2019-03-19 12:58:21 +0100

    Made changes to mgmotion

--- motionvideo/tests/test_mgmotion.py
+++ motionvideo/tests/test_mgmotion.py
@@ -1,12 +1,15 @@
 import sys
 sys.path.append('../..')
-from motionvideo.mgmotion import mg_motion
+from motionvideo.mgmotion import MgObject
 import cv2
 import numpy as np
 
 
+mg = MgObject('pianist.avi',filtertype = 'Regular',endtime = 5)
+mg.motionvideo()
 
-mg_motion('pianist.avi',endtime = 10)
+
+#mg_motion('pianist.avi',endtime = 10)
 """cap = cv2.VideoCapture('pianist.avi');
  
 fps = int(cap.get(cv2.CAP_PROP_FPS))
