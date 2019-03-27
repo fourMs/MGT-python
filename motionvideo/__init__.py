2fe9116f6fc58bda6f5fe637cbac21bb28cbbed1
fridafu <31306289+fridafu@users.noreply.github.com>
2019-03-27 14:33:54 +0100

    New module structure

--- motionvideo/__init__.py
+++
@@ -1,9 +0,0 @@
-from .version import __version__
-from .motionvideo import mgmotion
-
-
-# if somebody does "from somepackage import *", this is what they will
-# be able to access:
-__all__ = [
-    'mgmotion',
-]
