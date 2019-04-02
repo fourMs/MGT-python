b157b98424897d7a2db2b19d4847da72ab6f0e5f
Marcus Widmer <marcus.widmer@fys.uio.no>
2019-04-02 18:23:00 +0200

    Added find_motion_box() and constrainNumber

--- mgmodule/tests/test_mgmotion.py
+++ mgmodule/tests/test_mgmotion.py
@@ -2,5 +2,18 @@ import sys
 sys.path.append('../..')
 
 import mgmodule
-mg = mgmodule.MgObject('dance.avi',filtertype = 'Regular', color = True)
-mg.motionvideo()
+import cv2
+
+mg = mgmodule.MgObject('pianist.avi',filtertype = 'Regular', color = True,endtime = 3,thresh=0.3)
+print(mg.height,mg.width)
+img=cv2.imread('circle.png')
+img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+mg.find_motion_box(img,margin = 10)
+
+cv2.imshow('',mg.motion_box)
+cv2.waitKey(0)
+
+
+print(constrainNumber(11,0,10))
+
