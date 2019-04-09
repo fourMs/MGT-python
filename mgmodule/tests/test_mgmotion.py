156e26bee9fa4875e60d133517a4e45fae78b500
fridafu <31306289+fridafu@users.noreply.github.com>
2019-04-09 16:42:55 +0200

    separate functions for adjusting video before analysis

--- mgmodule/tests/test_mgmotion.py
+++ mgmodule/tests/test_mgmotion.py
@@ -4,16 +4,8 @@ sys.path.append('../..')
 import mgmodule
 import cv2
 
-mg = mgmodule.MgObject('pianist.avi',filtertype = 'Regular', color = True,endtime = 3,thresh=0.3)
-print(mg.height,mg.width)
-img=cv2.imread('circle.png')
-img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-mg.find_motion_box(img,margin = 10)
-
-cv2.imshow('',mg.motion_box)
-cv2.waitKey(0)
-
-
-print(constrainNumber(11,0,10))
+mg = mgmodule.MgObject('dance.avi', starttime = 5, endtime = 10, color = True, brightness = 90, contrast = 90, skip = 5)
+#mg.cropvideo()
+#mgc = mgmodule.MgObject('dance_cropped.avi')
+mg.motionvideo()
 
