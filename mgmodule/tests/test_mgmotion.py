8ba3795ece6fa9d5bd90bd5d1ddd38fca78d0d84
Marcus Widmer <marcus.widmer@fys.uio.no>
2019-06-26 13:33:06 +0200

    Moved history and average again

--- mgmodule/tests/test_mgmotion.py
+++ mgmodule/tests/test_mgmotion.py
@@ -1,15 +1,17 @@
 import sys
 import os
 sys.path.append('../..')
+sys.path.append('..')
+
 import mgmodule
-#from ._history import history
 import cv2
+from _history import history
+from _constrainNumber import constrainNumber
+
+
 
 
 mg = mgmodule.MgObject('dance.avi', starttime = 5, endtime = 10, color = True, brightness = 90, contrast = 90, skip = 1)
-print(mg.constrainNumber(3,4,7))
+print(constrainNumber(3,4,7))
 #mgc = mgmodule.MgObject('dance_cropped.avi')
-
-
-
 
