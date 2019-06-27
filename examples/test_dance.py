ddbec544bb31632441df64ec7f1e6b546be84ebd
alexarje <arjensenius@gmail.com>
2019-06-27 22:34:34 +0200

    Moving folder

---
+++ examples/test_dance.py
@@ -0,0 +1,17 @@
+import sys
+import os
+sys.path.append('../mgmodule')
+sys.path.append('..')
+import mgmodule
+import cv2
+
+#import any non-module functions you want to use (see documentation) this way:
+from _average import average_image
+
+#CREATE MODULE OBJECT: Here is an example call to create an mg Object, using loads of parameters
+mg = mgmodule.MgObject('dance.avi', starttime = 2, endtime = 7, color = True, brightness = 90, contrast = 90)
+#USE MODULE METHOD: To run the motionvideo analysis, run the function using your object
+mg.mg_motionvideo()
+#USE NON-MODULE FUNCTION, this one can find an average image of any video, here using the mb objects filename
+average_image(mg.filename)
+average_image('dance_trim_cb_motion.avi')
--- mgmodule/tests/test_pianist.py
+++ mgmodule/tests/test_pianist.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append('../..')
+sys.path.append('../mgmodule')
 sys.path.append('..')
 import mgmodule
 import cv2
@@ -9,7 +9,7 @@ import cv2
 from _average import average_image
 
 #CREATE MODULE OBJECT: Here is an example call to create an mg Object, using loads of parameters
-mg = mgmodule.MgObject('pianist.avi', crop='auto')
+mg = mgmodule.MgObject('pianist.avi', color=False, crop='auto')
 #USE MODULE METHOD: To run the motionvideo analysis, run the function using your object
 mg.mg_motionvideo(thresh=0.1)
 #USE NON-MODULE FUNCTION, this one can find an average image of any video, here using the mb objects filename
