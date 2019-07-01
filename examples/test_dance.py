2b7b63d45bee1202ab1a87ae9066095eae6d6076
Marcus Widmer <marcus.widmer@fys.uio.no>
2019-07-01 16:53:01 +0200

    Inverted motionvideo and inverted motionhistory, and updated documentation

--- examples/test_dance.py
+++ examples/test_dance.py
@@ -10,10 +10,10 @@ from _average import average_image
 from _history import history
 
 #CREATE MODULE OBJECT: Here is an example call to create an mg Object, using loads of parameters
-mg = mgmodule.MgObject('dance.avi', starttime = 2, endtime = 7, color = False, crop = 'auto')
+mg = mgmodule.MgObject('dance.avi', starttime = 2, endtime = 10, color = False, crop = 'Auto')
 #USE MODULE METHOD: To run the motionvideo analysis, run the function using your object
-mg.mg_motionvideo()
-
+mg.mg_motionhistory()
+mg.mg_motionvideo(inverted_motionvideo = True)
 #USE NON-MODULE FUNCTION, this one can find an average image of any video, here using the mb objects filename
 #average_image('dance_trim_cb_motion.avi')
 
