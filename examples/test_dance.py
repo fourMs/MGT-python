2a88a926464cf2cc1628034e1aba3fe715e96a98
Marcus Widmer <marcus.widmer@fys.uio.no>
2019-07-15 19:23:46 +0200

    Changed test_dance.py

--- examples/test_dance.py
+++ examples/test_dance.py
@@ -14,9 +14,9 @@ mg = mgmodule.MgObject('dance.avi', starttime = 2, endtime = 10, color = False, 
 #USE MODULE METHOD: To run the motionvideo analysis, run the function using your object
 mg.mg_motionvideo(inverted_motionvideo = False, inverted_motiongram = True, thresh=0.1, unit='seconds')
 #This runs the motion history on the motion video
-#mg.mg_motionhistory(history_length=25, thresh=0.01, inverted_motionhistory = False, blur='Average')
+mg.mg_motionhistory(history_length=25, thresh=0.01, inverted_motionhistory = False, blur='Average')
 
 #USE NON-MODULE FUNCTION, this one can find an average image of any video, here using the mb objects filename
-#average_image(mg.of+'_motion'+'.avi', enhance = 1)
+average_image(mg.of+'_motion'+'.avi', enhance = 1)
 
-#history(mg.of+'.avi',history_length=25)
+history(mg.of+'.avi',history_length=25)
