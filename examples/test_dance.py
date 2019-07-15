2579f487a074fdab4f20df9a2db990098e5e014a
Marcus Widmer <marcus.widmer@fys.uio.no>
2019-07-15 18:05:01 +0200

    Added enhance feature to average_image()

--- examples/test_dance.py
+++ examples/test_dance.py
@@ -10,13 +10,13 @@ from _average import average_image
 from _history import history
 
 #CREATE MODULE OBJECT: Here is an example call to create an mg Object, using loads of parameters
-mg = mgmodule.MgObject('dance.avi', starttime = 2, endtime = 15, color = True, contrast = 50, brightness = 50)
+mg = mgmodule.MgObject('dance.avi', starttime = 2, endtime = 15, color = False, contrast = 0, brightness = 0)
 #USE MODULE METHOD: To run the motionvideo analysis, run the function using your object
-mg.mg_motionvideo(inverted_motionvideo = False, inverted_motiongram = False, thresh=0.1)
+mg.mg_motionvideo(inverted_motionvideo = False, inverted_motiongram = True, thresh=0.1)
 #This runs the motion history on the motion video
 mg.mg_motionhistory(history_length=25, thresh=0.01, inverted_motionhistory = False, blur='Average')
 
 #USE NON-MODULE FUNCTION, this one can find an average image of any video, here using the mb objects filename
-average_image(mg.of+'.avi')
+average_image(mg.of+'_motion'+'.avi', enhance = 1)
 
 history(mg.of+'.avi',history_length=25)
