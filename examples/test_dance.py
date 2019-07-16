0a770bbf4febafdbda55ce5e39ddb8226780e867
alexarje <arjensenius@gmail.com>
2019-07-16 21:49:05 +0200

    Modifying to test new functionality

--- examples/test_dance.py
+++ examples/test_dance.py
@@ -10,13 +10,14 @@ from _average import average_image
 from _history import history
 
 #CREATE MODULE OBJECT: Here is an example call to create an mg Object, using loads of parameters
-mg = mgmodule.MgObject('dance.avi', starttime = 2, endtime = 10, color = False, contrast = 0, brightness = 0)
+mg = mgmodule.MgObject('dance.avi', starttime = 2, endtime = 20, color = True, contrast = 50, brightness = 50)
 #USE MODULE METHOD: To run the motionvideo analysis, run the function using your object
-mg.mg_motionvideo(inverted_motionvideo = False, inverted_motiongram = True, thresh=0.1, unit='seconds')
+mg.mg_motionvideo(inverted_motionvideo = False, inverted_motiongram = False, thresh=0.1, unit='seconds')
 #This runs the motion history on the motion video
 mg.mg_motionhistory(history_length=25, thresh=0.01, inverted_motionhistory = False, blur='Average')
 
 #USE NON-MODULE FUNCTION, this one can find an average image of any video, here using the mb objects filename
 average_image(mg.of+'_motion'+'.avi', enhance = 1)
+average_image(mg.filename)
 
 history(mg.of+'.avi',history_length=25)
--- examples/test_pianist.py
+++ examples/test_pianist.py
@@ -12,11 +12,12 @@ from _history import history
 #CREATE MODULE OBJECT: Here is an example call to create an mg Object, using loads of parameters
 mg = mgmodule.MgObject('pianist.avi', color=False, crop='auto', skip = 2)
 #USE MODULE METHOD: To run the motionvideo analysis, run the function using your object
-mg.mg_motionvideo(inverted_motionvideo = True, thresh=0.1)
+mg.mg_motionvideo(inverted_motionvideo = True, inverted_motiongram = True, thresh=0.1)
 #This runs the motion history on the motion video
 mg.mg_motionhistory()
 
 #USE NON-MODULE FUNCTION, this one can find an average image of any video, here using the mb objects filename
+average_image(mg.of+'_motion'+'.avi', enhance = 1)
 average_image(mg.filename)
 
 history(mg.of+'.avi',history_length=25)
