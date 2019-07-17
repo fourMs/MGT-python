51199ff74a27e1aba59bc3d0efd00c23c0ea5cf7
alexarje <arjensenius@gmail.com>
2019-07-17 22:02:43 +0200

    Testing new settings

--- examples/test_dance.py
+++ examples/test_dance.py
@@ -10,14 +10,24 @@ from _average import average_image
 from _history import history
 
 #CREATE MODULE OBJECT: Here is an example call to create an mg Object, using loads of parameters
-mg = mgmodule.MgObject('dance.avi', starttime = 2, endtime = 20, color = True, contrast = 50, brightness = 50)
+mg = mgmodule.MgObject('dance.avi', starttime = 2, endtime = 20, color = True, contrast = 100, brightness = 50)
+
 #USE MODULE METHOD: To run the motionvideo analysis, run the function using your object
-mg.mg_motionvideo(inverted_motionvideo = False, inverted_motiongram = False, thresh=0.1, unit='seconds')
+mg.mg_motionvideo(inverted_motionvideo = False, inverted_motiongram = False, thresh=0.05, unit='seconds')
+
 #This runs the motion history on the motion video
-#mg.mg_motionhistory(history_length=25, thresh=0.01, inverted_motionhistory = False, blur='Average')
+#mg.mg_motionhistory(history_length=50, thresh=0.05, inverted_motionhistory = False, blur='Average')
+
+history(mg.of+'.avi', history_length=25)
+history(mg.of+'_motion.avi', history_length=25)
 
 #USE NON-MODULE FUNCTION, this one can find an average image of any video, here using the mb objects filename
-#average_image(mg.of+'_motion'+'.avi', enhance = 1)
+
+# Average image of original video
 #average_image(mg.filename)
 
-#history(mg.of+'.avi',history_length=25)
+# Average image of pre-processed video
+average_image(mg.of+'.avi')
+
+# Average image of motion video
+average_image(mg.of+'_motion.avi')
--- examples/test_pianist.py
+++ examples/test_pianist.py
@@ -10,14 +10,19 @@ from _average import average_image
 from _history import history
 
 #CREATE MODULE OBJECT: Here is an example call to create an mg Object, using loads of parameters
-mg = mgmodule.MgObject('pianist.avi', color=False, crop='auto', skip = 2)
+mg = mgmodule.MgObject('pianist.avi', color=False, crop='auto', skip = 3)
 #USE MODULE METHOD: To run the motionvideo analysis, run the function using your object
 mg.mg_motionvideo(inverted_motionvideo = True, inverted_motiongram = True, thresh=0.1)
 #This runs the motion history on the motion video
-mg.mg_motionhistory()
+mg.mg_motionhistory(history_length=25, thresh=0.1, inverted_motionhistory = True, blur='Average')
 
 #USE NON-MODULE FUNCTION, this one can find an average image of any video, here using the mb objects filename
-average_image(mg.of+'_motion'+'.avi', enhance = 1)
-average_image(mg.filename)
 
-history(mg.of+'.avi',history_length=25)
+# Average image of original video
+#average_image(mg.filename)
+
+# Average image of pre-processed video
+average_image(mg.of+'.avi')
+
+# Average image of motion video
+average_image(mg.of+'_motion.avi')
