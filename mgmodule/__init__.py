d3d5be8d635e6455d8757330cb760861f92f52ef
Balint Laczko <45127463+balintlaczko@users.noreply.github.com>
2019-12-06 21:58:55 +0100

    added linebreaks in class doc
    Class documentation now shows up correctly in VS Code (and supposedly elsewhere as well)

--- mgmodule/__init__.py
+++ mgmodule/__init__.py
@@ -2,30 +2,35 @@ import os
 from ._input_test import mg_input_test
 from ._videoreader import mg_videoreader
 from ._constrainNumber import constrainNumber
+
+
 class MgObject:
     """ 
     Initializes Musical Gestures data structure from a given parameter video file.
 
-    Parameters:
-    filename (str): Name of input parameter video file.
-    method (str): Currently 'Diff' is the only implemented method. 
-    filtertype (str): 'Regular', 'Binary', 'Blob' (see function filterframe).
-    thresh (float): a number in [0,1]. Eliminates pixel values less than given threshold.
-    starttime (float): cut the video from this start time (min) to analyze what is relevant.
-    endtime (float): cut the video at this end time (min) to analyze what is relevant.
-    blur (str): 'Average' to apply a blurring filter, 'None' otherwise.
-    skip (int): When proceeding to analyze next frame of video, this many frames are skipped.
-    color (bool): True does the analysis in RGB, False in grayscale. 
-    contrast (float): apply +/- 100 contrast to video
-    brightness (float): apply +/- 100 brightness to video
-    crop (str): 'none', 'manual', 'auto' to select cropping of relevant video frame size
+    Parameters:\n
+    -----------\n
+    filename (str): Name of input parameter video file.\n
+    method (str): Currently 'Diff' is the only implemented method.\n 
+    filtertype (str): 'Regular', 'Binary', 'Blob' (see function filterframe).\n
+    thresh (float): A number in [0,1]. Eliminates pixel values less than given threshold.\n
+    starttime (float): Cut the video from this start time (min) to analyze what is relevant.\n
+    endtime (float): Cut the video at this end time (min) to analyze what is relevant.\n
+    blur (str): 'Average' to apply a blurring filter, 'None' otherwise.\n
+    skip (int): When proceeding to analyze next frame of video, this many frames are skipped.\n
+    color (bool): True does the analysis in RGB, False in grayscale.\n
+    contrast (float): Apply +/- 100 contrast to video.\n
+    brightness (float): Apply +/- 100 brightness to video.\n
+    crop (str): 'none', 'manual', 'auto' to select cropping of relevant video frame size.
     """
 
-    def __init__(self, filename, method = 'Diff', filtertype = 'Regular', thresh = 0.0001, starttime = 0, endtime = 0, blur = 'None', skip = 0, color = True, contrast = 0, brightness = 0, crop = 'None'):
+    def __init__(self, filename, method='Diff', filtertype='Regular', thresh=0.0001, starttime=0, endtime=0, blur='None', skip=0, color=True, contrast=0, brightness=0, crop='None'):
 
         self.filename = filename
-        self.of = os.path.splitext(self.filename)[0]   #name of file without extension (only-filename)
-        self.fex = os.path.splitext(self.filename)[1]  #file extension
+        # name of file without extension (only-filename)
+        self.of = os.path.splitext(self.filename)[0]
+        # file extension
+        self.fex = os.path.splitext(self.filename)[1]
         self.color = color
         self.method = method
         self.starttime = starttime
@@ -40,7 +45,6 @@ class MgObject:
         self.test_input()
         self.get_video()
 
-    
     from ._motionvideo import mg_motionvideo as motionvideo
     from ._motionvideo import plot_motion_metrics
     from ._cropvideo import mg_cropvideo, find_motion_box, find_total_motion_box
@@ -49,8 +53,10 @@ class MgObject:
 
     def test_input(self):
         """ Gives feedback to user if initialization from input went wrong. """
-        mg_input_test(self.filename, self.method, self.filtertype, self.thresh, self.starttime, self.endtime, self.blur, self.skip)
+        mg_input_test(self.filename, self.method, self.filtertype,
+                      self.thresh, self.starttime, self.endtime, self.blur, self.skip)
 
     def get_video(self):
         """ Creates a video attribute to the Musical Gestures object with the given correct settings. """
-        self.video, self.length, self.width, self.height, self.fps, self.endtime, self.of = mg_videoreader(self.filename, self.starttime, self.endtime, self.skip, self.contrast, self.brightness, self.crop)
+        self.video, self.length, self.width, self.height, self.fps, self.endtime, self.of = mg_videoreader(
+            self.filename, self.starttime, self.endtime, self.skip, self.contrast, self.brightness, self.crop)
