a4e9df38df12b88ae645800d6b9120a289c3ce47
Balint Laczko <45127463+balintlaczko@users.noreply.github.com>
2019-12-17 13:22:06 +0100

    show() is now able to show images as well, implemented keys for ease of use

--- mgmodule/_show.py
+++ mgmodule/_show.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
 import os
+from matplotlib import pyplot as plt
 
 
-def mg_show(self, filename=None, video=None):
+def mg_show(self, filename=None, key=None):
     """
 
     This function simply plays the current vidcap VideoObject. The speed of the video playback 
@@ -11,61 +12,88 @@ def mg_show(self, filename=None, video=None):
 
     Parameters:
 
-    - filename(str): If left empty, the current vidcap object is played. If filename is given,
-    this file is played instead. If either of the shorthands 'motion', 'history', or 'motionhistory' is used
-    the method attempts to show the (previously rendered) video file corresponding to the one in the MgObject.
+    - filename (str) : If left empty, the current vidcap object is played. If filename is given,
+    this file is played instead. 
+    - key (str) : {'mgx', 'mgy', 'average', 'plot', 'motion', 'history', 'motionhistory'} If either 
+    of the shorthands 'motion', 'history', or 'motionhistory' is used the method attempts to show 
+    the (previously rendered) video file corresponding to the one in the MgObject.
 
     """
+
+    video_mode = True
+
+    def show_image(ending, title=''):
+        video_mode = False
+        img = cv2.imread(self.of + ending, 3)
+        cv2.imshow(title, img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
     if filename == None:
-        if video == None:
+
+        if key == None:
             filename = self.of+self.fex
-        elif video == 'motion':
+        elif key == 'mgx':
+            show_image('_mgx.png', 'Horizontal Motiongram')
+        elif key == 'mgy':
+            show_image('_mgy.png', 'Vertical Motiongram')
+        elif key == 'average':
+            show_image('_average.png', 'Average')
+        elif key == 'plot':
+            show_image('_motion_com_qom.png',
+                       'Centroid and Quantity of Motion')
+
+        elif key == 'motion':
             if os.path.exists(self.of + '_motion' + self.fex):
                 filename = self.of + '_motion' + self.fex
             else:
                 print("No motion video found corresponding to",
                       self.of+self.fex, ". Try making one with .motion()")
-        elif video == 'history':
+        elif key == 'history':
             if os.path.exists(self.of + '_history' + self.fex):
                 filename = self.of + '_history' + self.fex
             else:
                 print("No history video found corresponding to",
                       self.of+self.fex, ". Try making one with .history()")
-        elif video == 'motionhistory':
+        elif key == 'motionhistory':
             if os.path.exists(self.of + '_motionhistory' + self.fex):
                 filename = self.of + '_motionhistory' + self.fex
             else:
                 print("No motion history video found corresponding to",
                       self.of+self.fex, ". Try making one with .motionhistory()")
         else:
-            print("Unknown shorthand. Try 'motion', 'history', or 'motionhistory'. Showing video from the MgObject.")
+            print("Unknown shorthand.\n",
+                  "For images, try 'mgx', 'mgy', 'average' or 'plot'.\n",
+                  "For videos try 'motion', 'history', or 'motionhistory'.\n",
+                  "Showing video from the MgObject.")
             filename = self.of+self.fex
 
-    vidcap = cv2.VideoCapture(filename)
-    fps = float(vidcap.get(cv2.CAP_PROP_FPS))
-    # Check if camera opened successfully
-    if (vidcap.isOpened() == False):
-        print("Error opening video stream or file")
-    i = int(np.round((1/fps)*1000))
+    if video_mode and (filename != None):
+        vidcap = cv2.VideoCapture(filename)
+        fps = float(vidcap.get(cv2.CAP_PROP_FPS))
+        # Check if camera opened successfully
+        if (vidcap.isOpened() == False):
+            print("Error opening video stream or file")
+        i = int(np.round((1/fps)*1000))
 
-    # Read until video is completed
-    while(vidcap.isOpened()):
-        # Capture frame-by-frame
-        ret, frame = vidcap.read()
-        if ret == True:
+        # Read until video is completed
+        while(vidcap.isOpened()):
+            # Capture frame-by-frame
+            ret, frame = vidcap.read()
+            if ret == True:
 
-            # Display the resulting frame
-            cv2.imshow('Frame', frame)
+                # Display the resulting frame
+                cv2.imshow('Frame', frame)
 
-            # Press Q on keyboard to  exit
-            if cv2.waitKey(i) & 0xFF == ord('q'):
+                # Press Q on keyboard to  exit
+                if cv2.waitKey(i) & 0xFF == ord('q'):
+                    break
+
+            # Break the loop
+            else:
                 break
+        # When everything done, release the video capture object
+        vidcap.release()
 
-        # Break the loop
-        else:
-            break
-    # When everything done, release the video capture object
-    vidcap.release()
-
-    # Closes all the frames
-    cv2.destroyAllWindows()
+        # Closes all the frames
+        cv2.destroyAllWindows()
