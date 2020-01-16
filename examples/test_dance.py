cd2b0490735ba3f674c67437c692f97738176bd3
Balint Laczko <45127463+balintlaczko@users.noreply.github.com>
2020-01-16 18:10:38 +0100

    updated examples
    #27

--- examples/test_dance.py
+++ examples/test_dance.py
@@ -5,29 +5,24 @@ sys.path.append('..')
 import mgmodule
 import cv2
 
-#import any non-module functions you want to use (see documentation) this way:
-from _average import average_image
-from _history import history
+# CREATE MODULE OBJECT: Here is an example call to create an mg Object, using loads of parameters
+mg = mgmodule.MgObject('dance.avi', starttime=2,
+                       endtime=20, contrast=100, brightness=50)
 
-#CREATE MODULE OBJECT: Here is an example call to create an mg Object, using loads of parameters
-mg = mgmodule.MgObject('dance.avi', starttime = 2, endtime = 20, color = True, contrast = 100, brightness = 50)
+# USE MODULE METHOD: To run the motionvideo analysis, run the function using your object
+mg.motion(inverted_motionvideo=False, inverted_motiongram=False,
+          thresh=0.05, unit='seconds')
 
-#USE MODULE METHOD: To run the motionvideo analysis, run the function using your object
-mg.mg_motionvideo(inverted_motionvideo = False, inverted_motiongram = False, thresh=0.05, unit='seconds')
-
-#This runs the motion history on the motion video
-#mg.mg_motionhistory(history_length=50, thresh=0.05, inverted_motionhistory = False, blur='Average')
-
-history(mg.of+'.avi', history_length=25)
-history(mg.of+'_motion.avi', history_length=25)
-
-#USE NON-MODULE FUNCTION, this one can find an average image of any video, here using the mb objects filename
+# History video
+mg.history(history_length=25)
+# Motion history video
+mg.history(mg.of+'_motion.avi', history_length=25)
 
 # Average image of original video
-#average_image(mg.filename)
+# mg.average('dance.avi')
 
 # Average image of pre-processed video
-average_image(mg.of+'.avi')
+mg.average()
 
 # Average image of motion video
-average_image(mg.of+'_motion.avi')
+mg.average(mg.of+'_motion.avi')
