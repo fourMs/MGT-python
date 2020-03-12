ac94cdd68d871d05848b53414fabdd1f0c0428c7
Balint Laczko <45127463+balintlaczko@users.noreply.github.com>
2020-03-12 17:46:54 +0100

    motion video export is now optional
    #40

--- mgmodule/_motionvideo.py
+++ mgmodule/_motionvideo.py
@@ -48,154 +48,175 @@ def mg_motionvideo(
     - An MgObject loaded with the resulting _motion video.
     """
 
-    self.blur = blur
-    self.method = method
-    self.thresh = thresh
-    self.filtertype = filtertype
+    if save_plot | save_data | save_motiongrams | save_video:
 
-    vidcap = cv2.VideoCapture(self.of+self.fex)
-    ret, frame = vidcap.read()
+        self.blur = blur
+        self.method = method
+        self.thresh = thresh
+        self.filtertype = filtertype
 
-    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    out = cv2.VideoWriter(self.of + '_motion' + self.fex,
-                          fourcc, self.fps, (self.width, self.height))
-
-    if save_motiongrams:
-        gramx = np.zeros([1, self.width, 3])
-        gramy = np.zeros([self.height, 1, 3])
-    if save_data | save_plot:
-        qom = np.array([])  # quantity of motion
-        com = np.array([])  # centroid of motion
-
-    ii = 0
-
-    if self.color == False:
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        if save_motiongrams:
-            gramx = np.zeros([1, self.width])
-            gramy = np.zeros([self.height, 1])
-
-    while(vidcap.isOpened()):
-        if self.blur == 'Average':
-            prev_frame = cv2.blur(frame, (10, 10))
-        elif self.blur == 'None':
-            prev_frame = frame
-
+        vidcap = cv2.VideoCapture(self.of+self.fex)
         ret, frame = vidcap.read()
-        if ret == True:
+
+        if save_video:
+            fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+            out = cv2.VideoWriter(self.of + '_motion' + self.fex,
+                                  fourcc, self.fps, (self.width, self.height))
+
+        if save_motiongrams:
+            gramx = np.zeros([1, self.width, 3])
+            gramy = np.zeros([self.height, 1, 3])
+        if save_data | save_plot:
+            qom = np.array([])  # quantity of motion
+            com = np.array([])  # centroid of motion
+
+        ii = 0
+
+        if self.color == False:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            if save_motiongrams:
+                gramx = np.zeros([1, self.width])
+                gramy = np.zeros([self.height, 1])
+
+        while(vidcap.isOpened()):
             if self.blur == 'Average':
-                # The higher these numbers the more blur you get
-                frame = cv2.blur(frame, (10, 10))
+                prev_frame = cv2.blur(frame, (10, 10))
             elif self.blur == 'None':
-                frame = frame  # No blur
+                prev_frame = frame
 
-            if self.color == True:
-                frame = frame
-            else:
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            ret, frame = vidcap.read()
+            if ret == True:
+                if self.blur == 'Average':
+                    # The higher these numbers the more blur you get
+                    frame = cv2.blur(frame, (10, 10))
+                elif self.blur == 'None':
+                    frame = frame  # No blur
 
-            frame = np.array(frame)
-            frame = frame.astype(np.int32)
-
-            if self.method == 'Diff':
                 if self.color == True:
-                    motion_frame_rgb = np.zeros([self.height, self.width, 3])
+                    frame = frame
+                else:
+                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-                    for i in range(frame.shape[2]):
+                frame = np.array(frame)
+                frame = frame.astype(np.int32)
+
+                if self.method == 'Diff':
+                    if self.color == True:
+                        motion_frame_rgb = np.zeros(
+                            [self.height, self.width, 3])
+
+                        for i in range(frame.shape[2]):
+                            motion_frame = (
+                                np.abs(frame[:, :, i]-prev_frame[:, :, i])).astype(np.uint8)
+                            motion_frame = filter_frame(
+                                motion_frame, self.filtertype, self.thresh, kernel_size)
+                            motion_frame_rgb[:, :, i] = motion_frame
+
+                        if save_motiongrams:
+                            movement_y = np.mean(motion_frame_rgb, axis=1).reshape(
+                                self.height, 1, 3)
+                            movement_x = np.mean(
+                                motion_frame_rgb, axis=0).reshape(1, self.width, 3)
+                            gramy = np.append(gramy, movement_y, axis=1)
+                            gramx = np.append(gramx, movement_x, axis=0)
+
+                    else:
                         motion_frame = (
-                            np.abs(frame[:, :, i]-prev_frame[:, :, i])).astype(np.uint8)
+                            np.abs(frame-prev_frame)).astype(np.uint8)
                         motion_frame = filter_frame(
                             motion_frame, self.filtertype, self.thresh, kernel_size)
-                        motion_frame_rgb[:, :, i] = motion_frame
 
-                    if save_motiongrams:
-                        movement_y = np.mean(motion_frame_rgb, axis=1).reshape(
-                            self.height, 1, 3)
-                        movement_x = np.mean(
-                            motion_frame_rgb, axis=0).reshape(1, self.width, 3)
-                        gramy = np.append(gramy, movement_y, axis=1)
-                        gramx = np.append(gramx, movement_x, axis=0)
+                        if save_motiongrams:
+                            movement_y = np.mean(
+                                motion_frame, axis=1).reshape(self.height, 1)
+                            movement_x = np.mean(
+                                motion_frame, axis=0).reshape(1, self.width)
+                            gramy = np.append(gramy, movement_y, axis=1)
+                            gramx = np.append(gramx, movement_x, axis=0)
 
-                else:
-                    motion_frame = (np.abs(frame-prev_frame)).astype(np.uint8)
-                    motion_frame = filter_frame(
-                        motion_frame, self.filtertype, self.thresh, kernel_size)
+                elif self.method == 'OpticalFlow':
+                    print('Optical Flow not implemented yet!')
 
-                    if save_motiongrams:
-                        movement_y = np.mean(
-                            motion_frame, axis=1).reshape(self.height, 1)
-                        movement_x = np.mean(
-                            motion_frame, axis=0).reshape(1, self.width)
-                        gramy = np.append(gramy, movement_y, axis=1)
-                        gramx = np.append(gramx, movement_x, axis=0)
+                if self.color == False:
+                    motion_frame = cv2.cvtColor(
+                        motion_frame, cv2.COLOR_GRAY2BGR)
+                    motion_frame_rgb = motion_frame
 
-            elif self.method == 'OpticalFlow':
-                print('Optical Flow not implemented yet!')
+                if save_video:
+                    if inverted_motionvideo:
+                        out.write(cv2.bitwise_not(
+                            motion_frame_rgb.astype(np.uint8)))
+                    else:
+                        out.write(motion_frame_rgb.astype(np.uint8))
 
-            if self.color == False:
-                motion_frame = cv2.cvtColor(motion_frame, cv2.COLOR_GRAY2BGR)
-                motion_frame_rgb = motion_frame
-
-            if inverted_motionvideo:
-                out.write(cv2.bitwise_not(motion_frame_rgb.astype(np.uint8)))
+                if save_plot | save_data:
+                    combite, qombite = centroid(motion_frame_rgb.astype(
+                        np.uint8), self.width, self.height)
+                    if ii == 0:
+                        com = combite.reshape(1, 2)
+                        qom = qombite
+                    else:
+                        com = np.append(com, combite.reshape(1, 2), axis=0)
+                        qom = np.append(qom, qombite)
             else:
-                out.write(motion_frame_rgb.astype(np.uint8))
 
-            if save_plot | save_data:
-                combite, qombite = centroid(motion_frame_rgb.astype(
-                    np.uint8), self.width, self.height)
-                if ii == 0:
-                    com = combite.reshape(1, 2)
-                    qom = qombite
-                else:
-                    com = np.append(com, combite.reshape(1, 2), axis=0)
-                    qom = np.append(qom, qombite)
+                mg_progressbar(self.length, self.length,
+                               pgbar_text, 'Complete')
+                break
+            ii += 1
+            pgbar_text = 'Rendering motion' + ", ".join(np.array(["-video", "-grams", "-plots", "-data"])[
+                np.array([save_video, save_motiongrams, save_plot, save_data])])
+            mg_progressbar(ii, self.length,
+                           pgbar_text, 'Complete')
+
+        if save_motiongrams:
+            if self.color == False:
+                # Normalize before converting to uint8 to keep precision
+                gramx = gramx/gramx.max()*255
+                gramy = gramy/gramy.max()*255
+                gramx = cv2.cvtColor(gramx.astype(
+                    np.uint8), cv2.COLOR_GRAY2BGR)
+                gramy = cv2.cvtColor(gramy.astype(
+                    np.uint8), cv2.COLOR_GRAY2BGR)
+
+            gramx = (gramx-gramx.min())/(gramx.max()-gramx.min())*255.0
+            gramy = (gramy-gramy.min())/(gramy.max()-gramy.min())*255.0
+
+            if equalize_motiongram:
+                gramx = gramx.astype(np.uint8)
+                gramx_hsv = cv2.cvtColor(gramx, cv2.COLOR_BGR2HSV)
+                gramx_hsv[:, :, 2] = cv2.equalizeHist(gramx_hsv[:, :, 2])
+                gramx = cv2.cvtColor(gramx_hsv, cv2.COLOR_HSV2BGR)
+
+                gramy = gramy.astype(np.uint8)
+                gramy_hsv = cv2.cvtColor(gramy, cv2.COLOR_BGR2HSV)
+                gramy_hsv[:, :, 2] = cv2.equalizeHist(gramy_hsv[:, :, 2])
+                gramy = cv2.cvtColor(gramy_hsv, cv2.COLOR_HSV2BGR)
+
+            if inverted_motiongram:
+                cv2.imwrite(self.of+'_mgx.png',
+                            cv2.bitwise_not(gramx.astype(np.uint8)))
+                cv2.imwrite(self.of+'_mgy.png',
+                            cv2.bitwise_not(gramy.astype(np.uint8)))
+            else:
+                cv2.imwrite(self.of+'_mgx.png', gramx.astype(np.uint8))
+                cv2.imwrite(self.of+'_mgy.png', gramy.astype(np.uint8))
+
+        if save_plot:
+            plot_motion_metrics(self.of, self.fps, com, qom,
+                                self.width, self.height, unit)
+
+        if save_data:
+            save_txt(self.of, com, qom, self.width, self.height, data_format)
+
+        if save_video:
+            return mgmodule.MgObject(self.of + '_motion' + self.fex)
         else:
-            mg_progressbar(self.length, self.length,
-                           'Rendering motion video:', 'Complete')
-            break
-        ii += 1
-        mg_progressbar(ii, self.length, 'Rendering motion video:', 'Complete')
+            return mgmodule.MgObject(self.of + self.fex)
 
-    if save_motiongrams:
-        if self.color == False:
-            # Normalize before converting to uint8 to keep precision
-            gramx = gramx/gramx.max()*255
-            gramy = gramy/gramy.max()*255
-            gramx = cv2.cvtColor(gramx.astype(np.uint8), cv2.COLOR_GRAY2BGR)
-            gramy = cv2.cvtColor(gramy.astype(np.uint8), cv2.COLOR_GRAY2BGR)
-
-        gramx = (gramx-gramx.min())/(gramx.max()-gramx.min())*255.0
-        gramy = (gramy-gramy.min())/(gramy.max()-gramy.min())*255.0
-
-        if equalize_motiongram:
-            gramx = gramx.astype(np.uint8)
-            gramx_hsv = cv2.cvtColor(gramx, cv2.COLOR_BGR2HSV)
-            gramx_hsv[:, :, 2] = cv2.equalizeHist(gramx_hsv[:, :, 2])
-            gramx = cv2.cvtColor(gramx_hsv, cv2.COLOR_HSV2BGR)
-
-            gramy = gramy.astype(np.uint8)
-            gramy_hsv = cv2.cvtColor(gramy, cv2.COLOR_BGR2HSV)
-            gramy_hsv[:, :, 2] = cv2.equalizeHist(gramy_hsv[:, :, 2])
-            gramy = cv2.cvtColor(gramy_hsv, cv2.COLOR_HSV2BGR)
-
-        if inverted_motiongram:
-            cv2.imwrite(self.of+'_mgx.png',
-                        cv2.bitwise_not(gramx.astype(np.uint8)))
-            cv2.imwrite(self.of+'_mgy.png',
-                        cv2.bitwise_not(gramy.astype(np.uint8)))
-        else:
-            cv2.imwrite(self.of+'_mgx.png', gramx.astype(np.uint8))
-            cv2.imwrite(self.of+'_mgy.png', gramy.astype(np.uint8))
-
-    if save_plot:
-        plot_motion_metrics(self.of, self.fps, com, qom,
-                            self.width, self.height, unit)
-
-    if save_data:
-        save_txt(self.of, com, qom, self.width, self.height, data_format)
-
-    return mgmodule.MgObject(self.of + '_motion' + self.fex)
+    else:
+        print("Nothing to render. Exiting...")
+        return mgmodule.MgObject(self.of + self.fex)
 
 
 def plot_motion_metrics(of, fps, com, qom, width, height, unit):
@@ -222,12 +243,29 @@ def plot_motion_metrics(of, fps, com, qom, width, height, unit):
 
 
 def save_txt(of, com, qom, width, height, data_format):
-    if data_format == "tsv":
-        np.savetxt(of+'_motion.tsv', np.append(np.append(qom.reshape(qom.shape[0], 1), (com[:, 0]/width).reshape(
-            com.shape[0], 1), axis=1), (com[:, 1]/height).reshape(com.shape[0], 1), axis=1), delimiter='\t')
-    elif data_format == "csv":
-        np.savetxt(of+'_motion.csv', np.append(np.append(qom.reshape(qom.shape[0], 1), (com[:, 0]/width).reshape(
-            com.shape[0], 1), axis=1), (com[:, 1]/height).reshape(com.shape[0], 1), axis=1), delimiter=',', fmt='%.15f')
-    elif data_format not in ["tsv", "csv"]:
-        print(
-            f"Invalid data format: '{data_format}'.\nFalling back to '.csv'.")
+    def save_single_file(of, com, qom, width, height, data_format):
+        if data_format == "tsv":
+            np.savetxt(of+'_motion.tsv', np.append(np.append(qom.reshape(qom.shape[0], 1), (com[:, 0]/width).reshape(
+                com.shape[0], 1), axis=1), (com[:, 1]/height).reshape(com.shape[0], 1), axis=1), delimiter='\t')
+        elif data_format == "csv":
+            np.savetxt(of+'_motion.csv', np.append(np.append(qom.reshape(qom.shape[0], 1), (com[:, 0]/width).reshape(
+                com.shape[0], 1), axis=1), (com[:, 1]/height).reshape(com.shape[0], 1), axis=1), delimiter=',', fmt='%.15f')
+        elif data_format == "txt":
+            np.savetxt(of+'_motion.txt', np.append(np.append(qom.reshape(qom.shape[0], 1), (com[:, 0]/width).reshape(
+                com.shape[0], 1), axis=1), (com[:, 1]/height).reshape(com.shape[0], 1), axis=1), delimiter=' ', fmt=['%d', '%.15f', '%.15f'])
+        elif data_format not in ["tsv", "csv", "txt"]:
+            print(
+                f"Invalid data format: '{data_format}'.\nFalling back to '.csv'.")
+
+    if type(data_format) == str:
+        save_single_file(of, com, qom, width, height, data_format)
+
+    elif type(data_format) == list:
+        if all([item in ["csv", "tsv", "txt"] for item in data_format]):
+            data_format = list(set(data_format))
+            [save_single_file(of, com, qom, width, height, item)
+             for item in data_format]
+        else:
+            print(
+                f"Unsupported formats in {data_format}.\nFalling back to '.csv'.")
+            save_single_file(of, com, qom, width, height, "csv")
