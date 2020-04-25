fde0a006f61117c0f2810080540a5c2b2e33e7c9
Balint Laczko <45127463+balintlaczko@users.noreply.github.com>
2020-04-25 18:35:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ README = (HERE / "README.md").read_text()
 setup(
     name='mgmodule',
     packages=['mgmodule'],
-    version='v1.0.5.2',
+    version='v1.0.5.3',
     license='GNU General Public License v3 (GPLv3)',
     description='Musical Gestures Toolbox for Python',
     long_description=README,
@@ -21,7 +21,7 @@ setup(
     author='University of Oslo fourMs Lab',
     author_email='a.r.jensenius@imv.uio.no',
     url='https://github.com/fourMs/MGT-python',
-    download_url='https://github.com/fourMs/MGT-python/archive/v1.0.5.2.tar.gz',
+    download_url='https://github.com/fourMs/MGT-python/archive/v1.0.5.3.tar.gz',
     keywords=['Computer Vision', 'Motion Analysis',
               'Musical Gestures', 'Video-Analysis'],
     install_requires=[
