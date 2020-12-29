017be7e21386a8f7fa1d0a743b45e82a1d750179
Balint Laczko <balintlaczko01@gmail.com>
2020-12-29 21:59:30 +0100

    videograms don't try to stream console output
    #179

--- musicalgestures/_videograms.py
+++ musicalgestures/_videograms.py
@@ -57,12 +57,12 @@ def videograms_ffmpeg(self):
         outname = self.of + '_skip_vgy.png'
         cmd = ['ffmpeg', '-y', '-i', shortened_file, '-vf',
                f'scale=1:{height}:sws_flags=area,normalize,tile={framecount}x1', '-aspect', f'{framecount}:{height}', '-frames', '1', outname]
-        ffmpeg_cmd(cmd, length, pb_prefix="Rendering horizontal videogram:")
+        ffmpeg_cmd(cmd, length, stream=False, pb_prefix="Rendering horizontal videogram:")
 
         outname = self.of + '_skip_vgx.png'
         cmd = ['ffmpeg', '-y', '-i', shortened_file, '-vf',
                f'scale={width}:1:sws_flags=area,normalize,tile=1x{framecount}', '-aspect', f'{width}:{framecount}', '-frames', '1', outname]
-        ffmpeg_cmd(cmd, length, pb_prefix="Rendering vertical videogram:")
+        ffmpeg_cmd(cmd, length, stream=False, pb_prefix="Rendering vertical videogram:")
 
         return MgList([MgImage(self.of+'_skip_vgx.png'), MgImage(self.of+'_skip_vgy.png')])
 
@@ -72,11 +72,11 @@ def videograms_ffmpeg(self):
         outname = self.of + '_vgy.png'
         cmd = ['ffmpeg', '-y', '-i', self.filename, '-frames', '1', '-vf',
                f'scale=1:{height}:sws_flags=area,normalize,tile={framecount}x1', '-aspect', f'{framecount}:{height}', outname]
-        ffmpeg_cmd(cmd, length, pb_prefix="Rendering horizontal videogram:")
+        ffmpeg_cmd(cmd, length, stream=False, pb_prefix="Rendering horizontal videogram:")
 
         outname = self.of + '_vgx.png'
         cmd = ['ffmpeg', '-y', '-i', self.filename, '-frames', '1', '-vf',
                f'scale={width}:1:sws_flags=area,normalize,tile=1x{framecount}', '-aspect', f'{width}:{framecount}', outname]
-        ffmpeg_cmd(cmd, length, pb_prefix="Rendering vertical videogram:")
+        ffmpeg_cmd(cmd, length, stream=False, pb_prefix="Rendering vertical videogram:")
 
         return MgList([MgImage(self.of+'_vgx.png'), MgImage(self.of+'_vgy.png')])
