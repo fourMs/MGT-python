ddc2d4c4aa130c30537510972d979d643011eb7f
Balint Laczko <balintlaczko01@gmail.com>
2021-05-25 18:24:21 +0200

    changed windows batch files for model download, fix minor md-problem
    #222

--- musicalgestures/pose/getCOCO_remote.bat
+++ musicalgestures/pose/getCOCO_remote.bat
@@ -4,12 +4,12 @@
 echo Downloading body pose (COCO) model...
 
 SET WGET_EXE=%1
-SET OPENPOSE_URL=http://posefs1.perception.cs.cmu.edu/OpenPose/models/
+SET OPENPOSE_URL=https://www.uio.no/ritmo/english/research/labs/fourms/software/musicalgesturestoolbox/mgt-python/pose-models/
 SET COCO_FOLDER=%2
 
 echo:
 
-SET COCO_MODEL=pose/coco/pose_iter_440000.caffemodel
-%WGET_EXE% -c %OPENPOSE_URL%%COCO_MODEL% -P %COCO_FOLDER%
+SET COCO_MODEL=coco/pose_iter_440000.caffemodel
+%WGET_EXE% -c %OPENPOSE_URL%%COCO_MODEL% -P %COCO_FOLDER% --no-check-certificate
 
 echo Download finished.
--- musicalgestures/pose/getMPI_remote.bat
+++ musicalgestures/pose/getMPI_remote.bat
@@ -4,12 +4,12 @@
 echo Downloading body pose (MPI) model...
 
 SET WGET_EXE=%1
-SET OPENPOSE_URL=http://posefs1.perception.cs.cmu.edu/OpenPose/models/
+SET OPENPOSE_URL=https://www.uio.no/ritmo/english/research/labs/fourms/software/musicalgesturestoolbox/mgt-python/pose-models/
 SET MPI_FOLDER=%2
 
 echo:
 
-SET MPI_MODEL=pose/mpi/pose_iter_160000.caffemodel
-%WGET_EXE% -c %OPENPOSE_URL%%MPI_MODEL% -P %MPI_FOLDER%
+SET MPI_MODEL=mpi/pose_iter_160000.caffemodel
+%WGET_EXE% -c %OPENPOSE_URL%%MPI_MODEL% -P %MPI_FOLDER% --no-check-certificate
 
 echo Download finished.
