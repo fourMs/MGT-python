96b0b41bb85c1e2de886240df01883870cc3a63d
Balint Laczko <balintlaczko01@gmail.com>
2021-05-25 18:40:56 +0200

    updated shell scripts for model download
    #222

--- musicalgestures/pose/getCOCO_remote.sh
+++ musicalgestures/pose/getCOCO_remote.sh
@@ -1,11 +1,11 @@
 # Downloading body pose (COCO) model...
 
-OPENPOSE_URL="http://posefs1.perception.cs.cmu.edu/OpenPose/models/"
+OPENPOSE_URL="https://www.uio.no/ritmo/english/research/labs/fourms/software/musicalgesturestoolbox/mgt-python/pose-models/"
 COCO_FOLDER="$1"
 
 #
 
-COCO_MODEL="pose/coco/pose_iter_440000.caffemodel"
-wget -c ${OPENPOSE_URL}${COCO_MODEL} -P ${COCO_FOLDER}
+COCO_MODEL="coco/pose_iter_440000.caffemodel"
+wget -c ${OPENPOSE_URL}${COCO_MODEL} -P ${COCO_FOLDER} --no-check-certificate
 
 # Download finished.
--- musicalgestures/pose/getMPI_here.sh
+++ musicalgestures/pose/getMPI_here.sh
@@ -1,9 +1,9 @@
 # Downloading body pose (MPI) model...
-OPENPOSE_URL="http://posefs1.perception.cs.cmu.edu/OpenPose/models/"
+OPENPOSE_URL="https://www.uio.no/ritmo/english/research/labs/fourms/software/musicalgesturestoolbox/mgt-python/pose-models/"
 MPI_FOLDER="mpi/"
 
 # 
-MPI_MODEL="pose/mpi/pose_iter_160000.caffemodel"
-wget -c ${OPENPOSE_URL}${MPI_MODEL} -P ${MPI_FOLDER}
+MPI_MODEL="mpi/pose_iter_160000.caffemodel"
+wget -c ${OPENPOSE_URL}${MPI_MODEL} -P ${MPI_FOLDER} --no-check-certificate
 
 # Download finished.
