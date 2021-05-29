5f27894dd9f17dcbf908b57ee043ef5b71116051
Balint Laczko <balintlaczko01@gmail.com>
2021-05-29 15:27:11 +0200

    deleting new lines
    #194

--- musicalgestures/pose/getCOCO_remote.sh
+++ musicalgestures/pose/getCOCO_remote.sh
@@ -1,11 +1,7 @@
 # Downloading body pose (COCO) model...
-
 OPENPOSE_URL="https://www.uio.no/ritmo/english/research/labs/fourms/software/musicalgesturestoolbox/mgt-python/pose-models/"
 COCO_FOLDER="$1"
-
 #
-
 COCO_MODEL="coco/pose_iter_440000.caffemodel"
 wget -c ${OPENPOSE_URL}${COCO_MODEL} -P ${COCO_FOLDER} --no-check-certificate
-
 # Download finished.
--- musicalgestures/pose/getMPI_remote.sh
+++ musicalgestures/pose/getMPI_remote.sh
@@ -1,9 +1,7 @@
 # Downloading body pose (MPI) model...
 OPENPOSE_URL="https://www.uio.no/ritmo/english/research/labs/fourms/software/musicalgesturestoolbox/mgt-python/pose-models/"
 MPI_FOLDER="$1"
-
 # 
 MPI_MODEL="mpi/pose_iter_160000.caffemodel"
 wget -c ${OPENPOSE_URL}${MPI_MODEL} -P ${MPI_FOLDER} --no-check-certificate
-
 # Download finished.
