cd8424e64ec874d8ddcbc87ed7490708341b98ed
Balint Laczko <balintlaczko01@gmail.com>
2021-08-28 12:57:42 +0200

    small fix in docstring and docs

--- musicalgestures/_history.py
+++ musicalgestures/_history.py
@@ -17,7 +17,7 @@ def history_ffmpeg(self, filename=None, history_length=10, weights=1, normalize=
     Args:
         filename (str, optional): Path to the input video file. If None, the video file of the MgObject is used. Defaults to None.
         history_length (int, optional): Number of frames to be saved in the history tail. Defaults to 10.
-        weights (int, float, list or str, optional): Defines the weight or weights applied to the frames in the history tail. If given as list the first element in the list will correspond to the weight of the newest frame in the tail. If given as a str - like "3 1.2 1" - it will be automatically converted to a list - like [3, 1.2, 1]. Defaults to 1.
+        weights (int/float/list/str, optional): Defines the weight or weights applied to the frames in the history tail. If given as list the first element in the list will correspond to the weight of the newest frame in the tail. If given as a str - like "3 1.2 1" - it will be automatically converted to a list - like [3, 1.2, 1]. Defaults to 1.
         normalize (bool, optional): If True, the history video will be normalized. This can be useful when processing motion (frame difference) videos. Defaults to False.
         norm_strength (int or float, optional): Defines the strength of the normalization where 1 represents full strength. Defaults to 1.
         norm_smooth (int, optional): Defines the number of previous frames to use for temporal smoothing. The input range of each channel is smoothed using a rolling average over the current frame and the `norm_smooth` previous frames. Defaults to 0.
