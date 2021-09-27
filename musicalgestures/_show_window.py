bcf9949c86f22e4950ce0dfb253ae47cc7320099
Balint Laczko <balintlaczko01@gmail.com>
2021-09-27 15:05:03 +0200

    added unwrap_str to make it work on ubuntu as well
    #238

--- musicalgestures/_show_window.py
+++ musicalgestures/_show_window.py
@@ -1,9 +1,10 @@
 import argparse
 import os
 import sys
+from musicalgestures._utils import unwrap_str
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Play video in a separate process')
     parser.add_argument('command', metavar='command', type=str, help='command')
     args = parser.parse_args()
-    os.system(args.command)
+    os.system(unwrap_str(args.command))
