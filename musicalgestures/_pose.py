1c019465f7b05cab5c02a7607783ba0ff6553cdf
Balint Laczko <balintlaczko01@gmail.com>
2021-11-13 19:59:11 +0100

    added body_25 model for pose
    #245

--- musicalgestures/_pose.py
+++ musicalgestures/_pose.py
@@ -11,17 +11,17 @@ import itertools
 
 
 def pose(
-    self, 
-    model='mpi', 
-    device='cpu', 
-    threshold=0.1, 
-    downsampling_factor=4, 
-    save_data=True, 
-    data_format='csv', 
-    save_video=True, 
-    target_name_video=None, 
-    target_name_data=None, 
-    overwrite=False):
+        self,
+        model='mpi',
+        device='cpu',
+        threshold=0.1,
+        downsampling_factor=4,
+        save_data=True,
+        data_format='csv',
+        save_video=True,
+        target_name_video=None,
+        target_name_data=None,
+        overwrite=False):
     """
     Renders a video with the pose estimation (aka. "keypoint detection" or "skeleton tracking") overlaid on it. 
     Outputs the predictions in a text file containing the normalized x and y coordinates of each keypoints 
@@ -59,6 +59,13 @@ def pose(
         nPoints = 18
         POSE_PAIRS = [[1, 0], [1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [
             8, 9], [9, 10], [1, 11], [11, 12], [12, 13], [0, 14], [0, 15], [14, 16], [15, 17]]
+    elif model.lower() == 'body_25':
+        protoFile = module_path + '/pose/body_25/pose_deploy.prototxt'
+        weightsFile = module_path + '/pose/body_25/pose_iter_584000.caffemodel'
+        model = 'body_25'
+        nPoints = 25
+        POSE_PAIRS = [[1, 8], [1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [8, 9], [9, 10], [10, 11], [8, 12], [12, 13], [
+            13, 14], [1, 0], [0, 15], [15, 17], [0, 16], [16, 18], [14, 19], [19, 20], [14, 21], [11, 22], [22, 23], [11, 24]]
     else:
         print(f'Unrecognized model "{model}", switching to default (mpi).')
         protoFile = module_path + '/pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt'
@@ -82,7 +89,7 @@ def pose(
     net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
     device = device.lower()
     # enforce CPU device in Colab
-    if in_colab() and device=='gpu':
+    if in_colab() and device == 'gpu':
         print('Sorry, OpenCV GPU acceleration is not supported in Colab. Switching to CPU.')
         device = 'cpu'
 
@@ -221,13 +228,17 @@ def pose(
                           'Right Knee', 'Right Ankle', 'Left Hip', 'Left Knee', 'Left Ankle', 'Right Eye', 'Left Eye', 'Right Ear', 'Left Ear']
             mpi_table = ['Head', 'Neck', 'Right Shoulder', 'Right Elbow', 'Right Wrist', 'Left Shoulder', 'Left Elbow',
                          'Left Wrist', 'Right Hip', 'Right Knee', 'Right Ankle', 'Left Hip', 'Left Knee', 'Left Ankle', 'Chest']
+            body_25_table = ['Nose', 'Neck', 'Right Shoulder', 'Right Elbow', 'Right Wrist', 'Left Shoulder', 'Left Elbow', 'Left Wrist', 'Mid Hip', 'Right Hip', 'Right Knee', 'Right Ankle', 'Left Hip',
+                             'Left Knee', 'Left Ankle', 'Right Eye', 'Left Eye', 'Right Ear', 'Left Ear', "Left Big Toe", "Left Small Toe", "Left Heel", "Right Big Toe", "Right Small Toe", "Right Heel"]
             headers = ['Time']
 
             table_to_use = []
             if model.lower() == 'mpi':
                 table_to_use = mpi_table
-            else:
+            elif model.lower() == 'coco':
                 table_to_use = coco_table
+            elif model.lower() == 'body_25':
+                table_to_use = body_25_table
 
             for i in range(len(table_to_use)):
                 header_x = table_to_use[i] + ' X'
@@ -245,7 +256,8 @@ def pose(
                     target_name_data = of+'_pose.tsv'
                 else:
                     # take name, but enforce tsv
-                    target_name_data = os.path.splitext(target_name_data)[0] + '.tsv'
+                    target_name_data = os.path.splitext(
+                        target_name_data)[0] + '.tsv'
                 if not overwrite:
                     target_name_data = generate_outfilename(target_name_data)
 
@@ -266,9 +278,10 @@ def pose(
                     target_name_data = of+'_pose.csv'
                 else:
                     # take name, but enforce csv
-                    target_name_data = os.path.splitext(target_name_data)[0] + '.csv'
+                    target_name_data = os.path.splitext(
+                        target_name_data)[0] + '.csv'
                 if not overwrite:
-                    target_name_data = generate_outfilename(target_name_data)                
+                    target_name_data = generate_outfilename(target_name_data)
 
                 df.to_csv(target_name_data, index=None)
 
@@ -278,9 +291,10 @@ def pose(
                     target_name_data = of+'_pose.txt'
                 else:
                     # take name, but enforce txt
-                    target_name_data = os.path.splitext(target_name_data)[0] + '.txt'
+                    target_name_data = os.path.splitext(
+                        target_name_data)[0] + '.txt'
                 if not overwrite:
-                    target_name_data = generate_outfilename(target_name_data)   
+                    target_name_data = generate_outfilename(target_name_data)
 
                 with open(target_name_data, 'wb') as f:
                     head_str = ''
@@ -293,11 +307,14 @@ def pose(
                         len(table_to_use)*2)]
                     np.savetxt(f, df.values, delimiter=' ', fmt=fmt_list)
             elif data_format not in ["tsv", "csv", "txt"]:
-                print(f"Invalid data format: '{data_format}'.\nFalling back to '.csv'.")
-                save_single_file(of, width, height, model, data, "csv", target_name_data=target_name_data, overwrite=overwrite)
+                print(
+                    f"Invalid data format: '{data_format}'.\nFalling back to '.csv'.")
+                save_single_file(of, width, height, model, data, "csv",
+                                 target_name_data=target_name_data, overwrite=overwrite)
 
         if type(data_format) == str:
-            save_single_file(of, width, height, model, data, data_format, target_name_data=target_name_data, overwrite=overwrite)
+            save_single_file(of, width, height, model, data, data_format,
+                             target_name_data=target_name_data, overwrite=overwrite)
 
         elif type(data_format) == list:
             if all([item.lower() in ["csv", "tsv", "txt"] for item in data_format]):
@@ -305,15 +322,19 @@ def pose(
                 [save_single_file(of, width, height, model, data, item, target_name_data=target_name_data, overwrite=overwrite)
                  for item in data_format]
             else:
-                print(f"Unsupported formats in {data_format}.\nFalling back to '.csv'.")
-                save_single_file(of, width, height, model, data, "csv", target_name_data=target_name_data, overwrite=overwrite)
+                print(
+                    f"Unsupported formats in {data_format}.\nFalling back to '.csv'.")
+                save_single_file(of, width, height, model, data, "csv",
+                                 target_name_data=target_name_data, overwrite=overwrite)
 
     if save_data:
-        save_txt(of, width, height, model, data, data_format, target_name_data=target_name_data, overwrite=overwrite)
+        save_txt(of, width, height, model, data, data_format,
+                 target_name_data=target_name_data, overwrite=overwrite)
 
     if save_video:
         # save result as pose_video for parent MgObject
-        self.pose_video = musicalgestures.MgObject(destination_video, color=self.color, returned_by_process=True)
+        self.pose_video = musicalgestures.MgObject(
+            destination_video, color=self.color, returned_by_process=True)
         return self.pose_video
     else:
         # otherwise just return the parent MgObject
@@ -338,29 +359,38 @@ def download_model(modeltype):
     pb_prefix = ''
     mpi_script = module_path + '/pose/getMPI'
     coco_script = module_path + '/pose/getCOCO'
-    wget_win = musicalgestures._utils.wrap_str(module_path + '/3rdparty/windows/wget/wget.exe')
-    target_folder_mpi = musicalgestures._utils.wrap_str(module_path + '/pose/mpi')
-    target_folder_coco = musicalgestures._utils.wrap_str(module_path + '/pose/coco')
+    body_25_script = module_path + '/pose/getBODY25'
+    wget_win = musicalgestures._utils.wrap_str(
+        module_path + '/3rdparty/windows/wget/wget.exe')
+    target_folder_mpi = musicalgestures._utils.wrap_str(
+        module_path + '/pose/mpi')
+    target_folder_coco = musicalgestures._utils.wrap_str(
+        module_path + '/pose/coco')
+    target_folder_body_25 = musicalgestures._utils.wrap_str(
+        module_path + '/pose/body_25')
 
     if the_system == 'Windows':
         mpi_script += batch
         coco_script += batch
+        body_25_script += batch
     elif in_colab():
         mpi_script += shell_colab
         coco_script += shell_colab
+        body_25_script += shell_colab
     else:
         mpi_script += shell
         coco_script += shell
+        body_25_script += shell
 
     if modeltype.lower() == 'mpi':
-        command = musicalgestures._utils.wrap_str(mpi_script) 
+        command = musicalgestures._utils.wrap_str(mpi_script)
         if the_system == 'Windows':
             command += f' {wget_win} {target_folder_mpi}'
         else:
             command = 'sudo -S bash ' + command
             command += f' {target_folder_mpi}'
         pb_prefix = 'Downloading MPI model:'
-    else:
+    elif modeltype.lower() == 'coco':
         command = coco_script
         if the_system == 'Windows':
             command += f' {wget_win} {target_folder_coco}'
@@ -368,6 +398,14 @@ def download_model(modeltype):
             command = 'sudo -S bash ' + command
             command += f' {target_folder_coco}'
         pb_prefix = 'Downloading COCO model:'
+    elif modeltype.lower() == 'body_25':
+        command = body_25_script
+        if the_system == 'Windows':
+            command += f' {wget_win} {target_folder_body_25}'
+        else:
+            command = 'sudo -S bash ' + command
+            command += f' {target_folder_body_25}'
+        pb_prefix = 'Downloading BODY_25 model:'
 
     pb = MgProgressbar(total=100, prefix=pb_prefix)
 
@@ -379,12 +417,12 @@ def download_model(modeltype):
             import getpass
             username = getpass.getuser()
             print(f'[sudo] password for {username}:')
-            p = getpass.getpass() 
+            p = getpass.getpass()
             process = subprocess.Popen(
                 command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE, universal_newlines=True, shell=True)
             process.stdin.write(p+'\n')
             process.stdin.flush()
-        except Exception as error: 
+        except Exception as error:
             print('ERROR', error)
 
     try:
