ef03e06a2c79d470f75531bd859355468a78453e
JP+ <63192177+joachimpoutaraud@users.noreply.github.com>
2022-06-17 17:58:43 +0200

    Fixing issues with pose in colab

--- musicalgestures/pose/getBODY_25_remote_colab.sh
+++ musicalgestures/pose/getBODY_25_remote_colab.sh
@@ -1,5 +1,3 @@
-# Downloading body pose (BODY_25) model...
-BODY_25_FOLDER="$1"
-# 
-wget -c "https://www.uio.no/ritmo/english/research/labs/fourms/software/musicalgesturestoolbox/mgt-python/pose-models/body25/pose_iter_584000.caffemodel" -P ${BODY_25_FOLDER} --no-check-certificate 
+# Downloading body pose (BODY_25) model... 
+wget -P "$1" "https://www.uio.no/ritmo/english/research/labs/fourms/software/musicalgesturestoolbox/mgt-python/pose-models/body25/pose_iter_584000.caffemodel" --no-check-certificate 
 # Download finished.
--- musicalgestures/pose/getCOCO_remote_colab.sh
+++ musicalgestures/pose/getCOCO_remote_colab.sh
@@ -1,6 +1,3 @@
 # Downloading body pose (COCO) model...
-OPENPOSE_URL="https://www.uio.no/ritmo/english/research/labs/fourms/software/musicalgesturestoolbox/mgt-python/pose-models/coco/pose_iter_440000.caffemodel"
-COCO_FOLDER="$1"
-#
-wget -c ${OPENPOSE_URL} -P ${COCO_FOLDER} --no-check-certificate 
+wget -P "$1" "https://www.uio.no/ritmo/english/research/labs/fourms/software/musicalgesturestoolbox/mgt-python/pose-models/coco/pose_iter_440000.caffemodel" --no-check-certificate 
 # Download finished.
