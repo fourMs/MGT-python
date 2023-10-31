ea17cc2548a692407ba0e8a83bd08aafe5509f49
JP+ <63192177+joachimpoutaraud@users.noreply.github.com>
2023-10-31 09:38:51 +0100

    Fix add coloured waveforms #159

--- musicalgestures/_audio.py
+++ musicalgestures/_audio.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import matplotlib
 import numpy as np
+
 from musicalgestures._utils import MgFigure, get_length, generate_outfilename, get_metadata, has_audio
+from musicalgestures._colored import MgAudioProcessor, MgWaveformImage
 
 # preventing librosa-matplotlib deadlock
 plt.plot()
@@ -46,22 +48,27 @@ class MgAudio:
 
     from musicalgestures._ssm import mg_ssm as ssm
 
-    def format_time(self, ax, format=True):
+    def format_time(self, ax, original_time=True, original_duration=None):
             """
             Format time for audio plotting of video file. This is useful if one wants to plot the original time of the video when frames have been skipped beforehand.
 
             Args:
                 ax (str, optional): Axis of the figure.
+                original_time (bool, optional): Whether to get the original time for audio plotting or not. Defaults to True.
+                original_duration (bool, optional): Whether to add the original duration of the file to be formatted manually. Defaults to None.
             """
             # Get original duration from video file
             try:
-                if format:
-                    original_duration = float(get_metadata(self.filename)[2]['TAG:title'])
+                if original_duration != None:
+                    original_duration = original_duration
                 else:
-                    original_duration = float(get_metadata(self.filename)[2]['duration'])
+                    if original_time:
+                        original_duration = float(get_metadata(self.filename)[2]['TAG:title'])
+                    else:
+                        original_duration = float(get_metadata(self.filename)[2]['duration'])
             except:
                 return 
-            
+
             time = np.round(np.linspace(0, original_duration, 10), 1)
 
             for i, v in enumerate(time):
@@ -73,21 +80,26 @@ class MgAudio:
                     minutes, sec = divmod(v, 60)
                     time[i] = '%02d.%02d' % (minutes, sec)
 
-            ax.xaxis.set_major_locator(ticker.MaxNLocator(prune='both', nbins=10))
+            ax.xaxis.set_major_locator(ticker.LinearLocator(numticks=10))
             if original_duration > 60:
                 ax.xaxis.set_major_formatter(ticker.FixedFormatter(list(map(lambda x: str(x).replace('.', ':'), list(time)))))
             else:  
                 ax.xaxis.set_major_formatter(ticker.FixedFormatter(list(time)))
 
-    def waveform(self, dpi=300, autoshow=True, raw=False, original_time=True, title=None, target_name=None, overwrite=False):
+    def waveform(self, dpi=300, autoshow=True, raw=False, colored=False, image_width=2500, image_height=500, fmin=500, fmax=None, cmap='freesound', original_time=True, title=None, target_name=None, overwrite=False):
         """
         Renders a figure showing the waveform of the video/audio file.
 
         Args:
             dpi (int, optional): Image quality of the rendered figure in DPI. Defaults to 300.
-            sr (int, optional): Sampling rate of the audio file. Defaults to 22050.
             autoshow (bool, optional): Whether to show the resulting figure automatically. Defaults to True.
             raw (bool, optional): Whether to show labels and ticks on the plot. Defaults to False.
+            colored (bool, optional): Whether to create a colored waveform image (freesound-style) from an audio input file. Defauts to False.
+            image_width (int, optional): Number of pixels for the colored waveform image width. Defaults to 2500.
+            image_height (int, optional): Number of pixels for the colored waveform image height. Defaults to 500.
+            fmin (int, optional): Minimum frequency for computing spectral centroid for the colored waveform image. Defaults to 500.
+            fmax (int, optional): Maximum frequency for computing spectral centroid for the colored waveform image. Defaults to None (i.e. Nyquist frequency).
+            cmap (str, optional): Colormap used for coloring the waveform, all colormaps included with matplotlib can be used. Defaults to 'freesound'.
             original_time (bool, optional): Whether to plot original time or not. This parameter can be useful if the video file has been shortened beforehand (e.g. skip). Defaults to True.
             title (str, optional): Optionally add title to the figure. Possible to set the filename as the title using the string 'filename'. Defaults to None.
             target_name (str, optional): The name of the output image. Defaults to None (which assumes that the input filename with the suffix "_waveform.png" should be used).
@@ -109,7 +121,22 @@ class MgAudio:
         if not overwrite:
             target_name = generate_outfilename(target_name)
 
-        y, sr = librosa.load(self.filename, sr=self.sr)
+        if colored:
+            # Process audio chunks and compute spectral centroid for creating the colored waveform
+            processor = MgAudioProcessor(self.filename, self.n_fft, fmin, fmax)
+            y = MgWaveformImage(image_width, image_height, cmap)
+            sr = processor.audio_file.samplerate
+
+            samples_per_pixel = processor.audio_file.frames / float(image_width)
+            
+            for x in range(image_width):
+                seek_point = int(x * samples_per_pixel)
+                next_seek_point = int((x + 1) * samples_per_pixel)
+                spectral_centroid = processor.spectral_centroid(seek_point) 
+                peaks = processor.peaks(seek_point, next_seek_point)        
+                y.draw_peaks(x, peaks, spectral_centroid) 
+        else:
+            y, sr = librosa.load(self.filename, sr=self.sr)
 
         fig, ax = plt.subplots(figsize=(12, 4), dpi=dpi)
         fig.patch.set_facecolor('white') # make sure background is white
@@ -122,17 +149,25 @@ class MgAudio:
             title = os.path.basename(self.filename)
         fig.suptitle(title, fontsize=16)
 
-        librosa.display.waveshow(y, sr=sr, ax=ax)
-        
-        # Adapt audio file plotting when skipping frames of a video file
-        self.format_time(ax, original_time)
+        if colored:
+            # Get the original duration of the audio file and format it to HH:MM:SS
+            original_duration = float(processor.audio_file.frames / processor.audio_file.samplerate)
+            self.format_time(ax, original_duration=original_duration)
+            ax.imshow(y.image.astype('uint8'), interpolation='nearest')
+            # Replace yticks with values between -1 and 1 for practicalities
+            ax.yaxis.set_major_locator(ticker.LinearLocator(numticks=len(ax.get_yticks())))
+            ax.yaxis.set_major_formatter(ticker.FixedFormatter(list(np.round(np.linspace(1,-1, len(ax.get_yticks())),1))))
+        else:
+            # Adapt audio file plotting when skipping frames of a video file
+            self.format_time(ax, original_time=original_time)
+            librosa.display.waveshow(y, sr=sr, ax=ax)
 
         if raw:
             fig.patch.set_visible(False)
             fig.suptitle('')
             ax.axis('off')
 
-        plt.tight_layout()    
+        fig.tight_layout()    
         plt.savefig(target_name, format='png', transparent=False)
 
         if not autoshow:
@@ -154,6 +189,7 @@ class MgAudio:
             image=target_name)
 
         return mgf
+
 
     def spectrogram(self, fmin=0.0, fmax=None, n_mels=128, power=2.0, top_db=80.0, dpi=300, autoshow=True, raw=False, original_time=False, title=None, target_name=None, overwrite=False):
         """
---
+++ musicalgestures/_colored.py
@@ -0,0 +1,243 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+import cv2
+import librosa
+import soundfile as sf
+from musicalgestures._utils import convert
+
+
+def get_max_level(filename, buffer_size):
+    max_level = 0
+    audio_file = sf.SoundFile(filename, 'r')
+    n_samples = audio_file.frames # total number of samples in audio file
+
+    while n_samples:
+        to_read = min(buffer_size, n_samples)
+        try:
+            samples = audio_file.read(to_read)
+        except RuntimeError: # this can happen with a broken header
+            break
+            
+        # Convert to mono by selecting left channel only
+        if audio_file.channels > 1:
+            samples = samples[:,0]          
+        max_level = max(max_level, np.abs(samples).max())
+        n_samples -= to_read
+
+    audio_file.close()
+    return max_level
+
+class MgAudioProcessor(object):
+    def __init__(
+            self, 
+            filename, 
+            n_fft,
+            fmin, 
+            fmax=None, 
+            window_function=np.hanning
+            ): 
+
+        """
+        The MgAudioProcessor class inherits from the MgAudio class for processing chunks of audio and calculating the spectral centroid 
+        and the peak samples in each chunk of audio.
+
+        Adapted from https://github.com/endolith/freesound-thumbnailer/blob/master/processing.py
+
+        Args:
+            filename (str): Path to the audio file. 
+            n_fft (int, optional): Length of the FFT window.
+            fmin (int): Minimum frequency for computing spectral centroid. 
+            fmax (int): Maximum frequency for computing spectral centroid. Defaults to None.
+            window_function (int, optional): Type of window to apply on chunks of audio. Defaults to np.hanning.
+        """
+        try: # check if it is an audio file or convert it to .wav format
+            self.audio_file = sf.SoundFile(filename, 'r')
+        except RuntimeError:
+            filename = convert(filename, filename + '.wav')
+            self.audio_file = sf.SoundFile(filename, 'r')
+
+        self.n_fft = n_fft
+        
+        self.fmin = fmin
+        if fmax == None:
+            self.fmax = self.audio_file.samplerate // 2
+        else:
+            fmax = self.fmax
+        
+        self.fmin_log = math.log10(self.fmin)
+        self.fmax_log = math.log10(self.fmax)
+        self.clip = lambda val, low, high: min(high, max(low, val))
+        
+        self.window = window_function(self.n_fft)
+        # Figure out what the maximum value is for an FFT doing the FFT of a DC signal
+        max_fft = (np.abs(np.fft.rfft(np.ones(self.n_fft) * self.window))).max()
+        # Set the scale to normalized audio and normalized FFT
+        max_level = get_max_level(filename, buffer_size=self.n_fft)
+        self.scale = 1.0 / max_level / max_fft if max_level > 0 else 1
+
+    def read_samples(self, start, size, resize_if_less=False):
+        """ 
+        Read size samples starting at start, if resize_if_less is True and less than size
+        samples are read, resize the array to size and fill with zeros 
+        """
+        
+        # Number of zeros to add to start and end of the buffer
+        add_to_start = 0
+        add_to_end = 0
+        
+        if start < 0:
+            # First FFT window starts centered around zero
+            if size + start <= 0:
+                return np.zeros(size) if resize_if_less else np.array([])
+            else:
+                self.audio_file.seek(0)
+                add_to_start = -start # remember: start is negative!
+                to_read = size + start
+
+                if to_read > self.audio_file.frames:
+                    add_to_end = to_read - self.audio_file.frames
+                    to_read = self.audio_file.frames
+        else:            
+            self.audio_file.seek(int(start))
+        
+            to_read = size
+            if start + to_read >= self.audio_file.frames:
+                to_read = self.audio_file.frames - start
+                add_to_end = size - to_read
+        
+        try:            
+            samples = self.audio_file.read(int(to_read))
+        except RuntimeError: # this can happen for wave files with broken headers
+            return np.zeros(size) if resize_if_less else np.zeros(2)
+
+        # Convert to mono by selecting left channel only
+        if self.audio_file.channels > 1:
+            samples = samples[:,0]
+
+        if resize_if_less and (add_to_start > 0 or add_to_end > 0):
+            if add_to_start > 0:   
+                samples = np.concatenate((np.zeros(int(add_to_start)), samples))
+            if add_to_end > 0:
+                samples = np.resize(samples, size)
+                samples[int(size - add_to_end):] = 0
+                
+        return samples
+    
+    def spectral_centroid(self, seek_point):
+        """ 
+        Starting at seek_point to read n_fft samples and calculate the spectral centroid 
+        """
+        
+        samples = self.read_samples(seek_point - self.n_fft // 2, self.n_fft, True) * self.window
+        # Compute spectral centroid
+        spectral_centroid = librosa.feature.spectral_centroid(y=samples, sr=self.audio_file.samplerate, n_fft=self.n_fft)[0].sum()
+        # Clip spectral centroid to desired frequency range
+        clip_centroid = math.log10(self.clip(spectral_centroid, self.fmin, self.fmax))
+        # Scale desired frequency range from 0 to 1
+        spectral_centroid = (clip_centroid - self.fmin_log) / (self.fmax_log - self.fmin_log)
+        
+        return (spectral_centroid)
+
+
+    def peaks(self, start_seek, end_seek, block_size=1024):
+        """ 
+        Read all samples between start_seek and end_seek, then find the minimum and maximum peak
+        in that range. Returns that pair in the order they were found. So if min was found first,
+        it returns (min, max) else the other way around. 
+        """
+        
+        min_index, max_index, min_value, max_value = -1, -1, 1, -1 
+    
+        if end_seek > self.audio_file.frames:
+            end_seek = self.audio_file.frames
+    
+        if block_size > end_seek - start_seek:
+            block_size = end_seek - start_seek
+        
+        for i in range(start_seek, end_seek, block_size):
+            samples = self.read_samples(i, block_size)
+    
+            local_max_index = np.argmax(samples)
+            local_max_value = samples[local_max_index]
+    
+            if local_max_value > max_value:
+                max_value = local_max_value
+                max_index = local_max_index
+    
+            local_min_index = np.argmin(samples)
+            local_min_value = samples[local_min_index]
+            
+            if local_min_value < min_value:
+                min_value = local_min_value
+                min_index = local_min_index
+    
+        return (min_value, max_value) if min_index < max_index else (max_value, min_value)
+
+class MgWaveformImage(object):
+    def __init__(self, image_width=2500, image_height=500, cmap='freesound'):
+
+        """
+        Given peaks and spectral centroids from the MgAudioProcessor class, the MgWaveformImage class will construct
+        a wavefile image using OpenCV.
+
+        Adapted from https://github.com/endolith/freesound-thumbnailer/blob/master/processing.py
+
+        Args:
+            image_width (int, optional): Number of pixels for the image width. Defaults to 2500.
+            image_height (int, optional): Number of pixels for the image height. Defaults to 500.
+            cmap (str, optional): Colormap used for coloring the waveform, all colormaps included with matplotlib can be used. Defaults to 'freesound'.
+        """
+        if cmap == 'freesound': # create original freesound colormap 
+            colors = [(50,0,200),(0,220,80),(255,224,0),(255,70,0)]
+            self.color_lookup = self.interpolate_colors(colors)
+        else: # use colormaps included with matplotlib
+            colors = plt.get_cmap(cmap)
+            colors = colors(np.arange(256))[...,:3]
+            self.color_lookup = colors * 255
+
+        self.image = np.zeros((image_height, image_width, 3))
+        self.image_width = image_width
+        self.image_height = image_height
+        
+    def draw_peaks(self, x, peaks, spectral_centroid):
+        """ 
+        Draw 2 peaks at x using the spectral_centroid for color 
+        """
+
+        y1 = int(self.image_height * 0.5 - peaks[0] * (self.image_height) * 0.5)
+        y2 = int(self.image_height * 0.5 - peaks[1] * (self.image_height) * 0.5)
+        
+        line_color = self.color_lookup[int(spectral_centroid * 255)] 
+        cv2.line(self.image, (x, y1), (x, y2), line_color)
+        
+    def interpolate_colors(self, colors, flat=False, num_colors=256):
+        """ 
+        Given a list of colors, create a larger list of colors linearly interpolating
+        the first one. If flatten is True a list of numbers will be returned. If
+        False, a list of (r,g,b) tuples. num_colors is the number of colors wanted
+        in the final list 
+        """
+
+        cmap = []
+        for i in range(num_colors):
+            index = (i * (len(colors) - 1)) / (num_colors - 1.0) 
+            index_int = int(index)
+            alpha = index - float(index_int)
+
+            if alpha > 0:
+                r = (1.0 - alpha) * colors[index_int][0] + alpha * colors[index_int + 1][0]
+                g = (1.0 - alpha) * colors[index_int][1] + alpha * colors[index_int + 1][1]
+                b = (1.0 - alpha) * colors[index_int][2] + alpha * colors[index_int + 1][2]
+            else:
+                r = (1.0 - alpha) * colors[index_int][0]
+                g = (1.0 - alpha) * colors[index_int][1]
+                b = (1.0 - alpha) * colors[index_int][2]
+
+            if flat:
+                cmap.extend((int(r), int(g), int(b)))
+            else:
+                cmap.append((int(r), int(g), int(b)))
+
+        return cmap
